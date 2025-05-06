a19b44dd6de1304c6c7933a0f48a4840575eb8ca
Zacatenango <44513116+Zacatenango@users.noreply.github.com>
2025-05-06 12:24:24 -0600

    Poniendo la configuración de la aplicación nueva donde sí va

--- dashboard/firebase-config.js
+++ dashboard/firebase-config.js
@@ -1,13 +1,14 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
     // TODO: Replace with your Firebase configuration
-    apiKey: "your-api-key",
-    authDomain: "your-auth-domain",
-    databaseURL: "your-database-url",
-    projectId: "your-project-id",
-    storageBucket: "your-storage-bucket",
-    messagingSenderId: "your-messaging-sender-id",
-    appId: "your-app-id"
+    apiKey: "AIzaSyArRfYoAEW946F9aIsnzht3PNoqSIc9k3Q",
+    authDomain: "misaplicaciones-a39bb.firebaseapp.com",
+    databaseURL: "https://misaplicaciones-a39bb-default-rtdb.firebaseio.com",
+    projectId: "misaplicaciones-a39bb",
+    storageBucket: "misaplicaciones-a39bb.firebasestorage.app",
+    messagingSenderId: "375780546756",
+    appId: "1:375780546756:web:e00c3de48e1e0de769d191",
+    measurementId: "G-0X2BWZM5HS"
 };
 
 // Initialize Firebase
